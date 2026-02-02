66ce467531a39a03e8c88db8bf51a2907f17569f
Steven Farkas <farkas.stev@gmail.com>
2026-02-02 11:24:33 -0500

    Added full directory copying and generation. Added to github docs

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from markdown_to_html import markdown_to_html_node
 
@@ -18,19 +19,21 @@ def copy_files(source_dir, destination_dir):
             copy_files(contents, os.path.join(destination_dir, contents))
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(
         f"Generating page from {from_path} to {dest_path} using template: {template_path}"
     )
-
     with open(from_path, "r") as f:
         markdown = f.read()
     with open(template_path, "r") as f:
         template = f.read()
     title = extract_title(markdown)
     html = markdown_to_html_node(markdown).to_html()
-    full_html_page = template.replace("{{ Title }}", title).replace(
-        "{{ Content }}", html
+    full_html_page = (
+        template.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html)
+        .replace('href="/', f'href="{base_path}')
+        .replace('src="/', f'src="{base_path}')
     )
 
     if not os.path.exists(os.path.dirname(dest_path)):
@@ -46,9 +49,30 @@ def extract_title(markdown):
     raise Exception("No title found")
 
 
+def generate_pages_recursive(from_path, template_path, dest_path, base_path):
+    if os.path.isfile(from_path):
+        des_path = os.path.join(
+            os.path.dirname(dest_path),
+            os.path.basename(from_path).replace(".md", ".html"),
+        )
+        generate_page(from_path, template_path, des_path, base_path)
+    elif os.path.isdir(from_path):
+        for item in os.listdir(from_path):
+            generate_pages_recursive(
+                os.path.join(from_path, item),
+                template_path,
+                os.path.join(dest_path, item),
+                base_path,
+            )
+
+
 def main():
-    copy_files("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    base_path = "/"
+    if len(sys.argv) == 2:
+        base_path = sys.argv[1]
+
+    copy_files("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", base_path)
 
 
 main()
