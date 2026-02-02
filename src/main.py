ff2fd3550cd80f3b1b13577fef66b0fe5348aa2d
Steven Farkas <farkas.stev@gmail.com>
2026-02-02 06:55:32 -0500

    Added file copying

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
+
+def copy_files(source_dir, destination_dir):
+    # Implement file copying logic here
+    if os.path.exists(destination_dir):
+        shutil.rmtree(destination_dir)
+    shutil.copytree(source_dir, destination_dir)
+    for contents in os.listdir(source_dir):
+        if os.path.isfile(contents):
+            print(f"Copying file: {contents}")
+            shutil.copy(contents, destination_dir)
+        elif os.path.isdir(contents):
+            print(f"Copying directory: {contents}")
+            copy_files(contents, os.path.join(destination_dir, contents))
+
+
 def main():
-    testNode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(testNode)
-
-
+    copy_files("static", "public")
 
 
 main()
